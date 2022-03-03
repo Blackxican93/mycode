ff47a3923bf8cd858a934868e8fc280502e6b6b5
Vernon Stephens <Blackxican93@users.noreply.github.com>
2022-03-03 17:07:27 +0000

    join and split

---
+++ quiz.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#Prompt the user to input answer
+
+
+print("What is your favorite programming languate?")
+answer = input("My favorite programming language is " + input")
+
+
