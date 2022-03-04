2e56fe69e234de4a7c7c52b1e796e33ebbc173b2
Vernon Stephens <Blackxican93@users.noreply.github.com>
2022-03-04 16:13:08 +0000

    studying for logic

---
+++ introfor/forloop3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def main():
+
+    """I have a..."""
+
+    #farm list
+
+    farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+    #for loop
+    for farm in farms:
+        print(farm.get("name", "Unknown Farm"), end=":\n")
+
+        for agri in farm.get("agriculture"):
+            print(" -", agri)
+
+    if __name__ == "__main__":
+        main()
+
